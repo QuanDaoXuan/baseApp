8677515d5191199cdc2fd107fc91643ed71434ca
quandx <quandx@kaopiz.com>
2020-07-10 09:42:19 +0700

    Initial Commit

---
+++ newProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newProject
+//
+//  Created by resopt on 7/10/20.
+//  Copyright © 2020 resopt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
